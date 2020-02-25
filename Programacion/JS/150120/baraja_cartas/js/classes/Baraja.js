4bd3456461e352789271515baa7c904f2f98b597
Oscar <Terres>
2020-02-25 15:31:34 +0100

    untracked

--- Programacion/JS/150120/baraja_cartas/js/classes/Baraja.js
+++ Programacion/JS/150120/baraja_cartas/js/classes/Baraja.js
@@ -1,9 +1,23 @@
-function Baraja (name){
+/* function Baraja (name){
 	this.name =name;
 	this.palo = Math.floor((Math.random() * 4) + 1);
 	this.posicion = Math.floor((Math.random() * 40) + 1);
 	this.repartida = false;
-	
+	 */
+
+
+	 function Baraja(cartasBastos,cartasOros,cartasCopas,cartasEspadas){
+
+		this.cartasBastos = cartasBastos;
+		this.cartasOros =cartasOros;
+		this.cartasCopas=cartasCopas;
+		this.cartasEspadas=cartasEspadas;
+
+		//metodos
+	Baraja.prototype.crearBaraja = function(){
+		let barajaCompleta = new Array (this.cartasBastos,this.cartasOros,this.cartasCopas,this.cartasEspadas);
+		return barajaCompleta;
+	}
 
 	Baraja.prototype.barajar = function(){
 		this.palo=Math.floor((Math.random() * 4) + 1);
@@ -22,4 +36,9 @@ function Baraja (name){
 		console.log("ẗoma carta");
 		return Baraja[this.palo][this.posicion];
 	}
+	
+	Baraja.prototype.barajarCartas = function (){
+		
+	}
+	
 }
