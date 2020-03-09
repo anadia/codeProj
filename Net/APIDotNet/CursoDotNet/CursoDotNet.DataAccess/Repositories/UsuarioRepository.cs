94beeae8ab349b0a1d6a14ece57c330913bf9e94
Oscar Terres <oterres@uma.es>
2020-03-09 19:58:41 +0100

    Proyecto .NET con token

--- Net/APIDotNet/CursoDotNet/CursoDotNet.DataAccess/Repositories/UsuarioRepository.cs
+++ Net/APIDotNet/CursoDotNet/CursoDotNet.DataAccess/Repositories/UsuarioRepository.cs
@@ -1,7 +1,17 @@
-﻿namespace CursoDotNet.DataAccess.Repositories
+﻿using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
+using CursoDotNet.Application.Services;
+
+namespace CursoDotNet.DataAccess.Repositories
 {
-    public class UsuarioRepository
+    public class UsuarioRepository :IUsuarioRepository
     {
-        
+        private readonly ApplicationDbContext _;
+
     }
+
+    public UsuarioRepository{
+    _dbContext = context ?? ThrowStatement new ArgumentNullException(nameof(context));
+    }
+
+    public
 }
